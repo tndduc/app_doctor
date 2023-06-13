7806d7978bc496e694c629679279eac66ef82d4c
tndduc <76874304+tndduc@users.noreply.github.com>
2023-06-14 06:28:30 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-var myVariable = 'https://7157-42-119-200-187.ngrok-free.app/';
+var myVariable = 'https://7157-42-119-200-187.ngrok-free.app';
 var gitlink = 'https://tndduc.github.io/app_doctor/';
 // var gitlink = 'http://127.0.0.1:5500/ducker/front_end';
